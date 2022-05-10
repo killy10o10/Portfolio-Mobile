540456288dc017d02a76efcd748dc0bff2567df3
killy10o10 <killynelly@gmail.com>
2022-05-10 02:25:05 +0000

    Add javaScript click event

---
+++ app.js
@@ -0,0 +1,6 @@
+const hamburger = document.getElementById('hamburger');
+const menu = document.getElementById('menu');
+
+hamburger.addEventListener('click', () => {
+  menu.classList.toggle('show')
+})
